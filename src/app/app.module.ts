3b9e9e38fb6e9f3f5f6329c67ae9658ad5eea957
Sam <sam.markey1500@gmail.com>
2022-12-03 18:34:59 +0000

    added angular basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
